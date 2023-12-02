a9ffbc59b13fd4c96a31863f748852b132a54177
jamiegyoung <jamiegyoung22@gmail.com>
2023-12-02 00:33:29 +0000

    Update day_1.go add part 2 solution

--- go/solutions/day_1.go
+++ go/solutions/day_1.go
@@ -8,27 +8,53 @@ import (
 )
 
 func Day_1() {
-	var lines = Input()
-	var cumSum = 0
+	numberStrings := map[string]int{
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	longestNumberStringCount := 5
+	smallestNumberStringCount := 3
+
+	lines := Input()
+	cumSum := 0
 
 	for _, line := range lines {
-		var first int
-		var second int
+		numericalLettersAcc := []int{}
 
-		for _, char := range strings.Split(line, "") {
-			i, err := strconv.Atoi(char)
-			if err != nil {
+		for i := 0; i < len(strings.Split(line, "")); i++ {
+			charAsString := string(line[i])
+
+			foundNumber, err := strconv.Atoi(charAsString)
+			if err == nil {
+				// handle numbers
+				numericalLettersAcc = append(numericalLettersAcc, foundNumber)
 				continue
 			}
-			if first == 0 {
-				first = i
-			}
 
-			second = i
+			// handle letters
+			for j := smallestNumberStringCount; j <= longestNumberStringCount; j++ {
+				if i+j > len(line) {
+					continue
+				}
+
+				subString := line[i : i+j]
+				if numberStrings[subString] != 0 {
+					numericalLettersAcc = append(numericalLettersAcc, numberStrings[subString])
+					continue
+				}
+			}
 		}
 
-		fmt.Println("first:", first, "second:", second)
-		joined, err := strconv.Atoi(fmt.Sprintf("%d%d", first, second))
+		joined, err := strconv.Atoi(fmt.Sprintf("%d%d", numericalLettersAcc[0], numericalLettersAcc[len(numericalLettersAcc)-1]))
 		if err != nil {
 			fmt.Println("An error occured parsing the joined numbers")
 			os.Exit(1)
@@ -39,4 +65,3 @@ func Day_1() {
 	fmt.Println()
 	fmt.Println(cumSum)
 }
-
